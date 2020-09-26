7c43f008792eb8ba9790e0bea1d7860b9cfc7481
siva <shivajstme@gmail.com>
2020-09-26 12:12:01 +1000

    Added Socket.io

--- server/index.js
+++ server/index.js
@@ -10,7 +10,17 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection',(socket)=>{
+    console.log('A user joined the connection ');
+
+    socket.on('disconnect',()=>{
+            console.log('User Just disconnected');
+    })
+})
+
 app.use(router);
+
+
 server.listen(PORT, ()=>{
     console.log(`Server has running on port ${PORT}`);
-})
+});
